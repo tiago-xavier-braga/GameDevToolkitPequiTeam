72ac4c00b066ea9104bc58deba51c2fdca6d9734
Tiago Braga <braga.tiagoxavier@gmail.com>
2023-01-21 21:13:48 -0300

    Created moviment for walls

---
+++ Assets/Runtime/Scripts/WallController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class WallController : MonoBehaviour
+{
+    [SerializeField] public float speed = 400;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+
+        rb.velocity = Vector2.left * speed * Time.deltaTime;
+    }
+}
