75b6f7a93e65807967ed54dd90554dc04ebaa554
Tiago Braga <braga.tiagoxavier@gmail.com>
2023-01-21 22:19:30 -0300

    creating movimento and destroy for walls

--- Assets/Runtime/Scripts/Walls/WallSpawn.cs
+++ Assets/Runtime/Scripts/Walls/WallSpawn.cs
@@ -13,13 +13,25 @@ public class WallSpawn : MonoBehaviour
 
     private void Start()
     {
-        firstWallTop = GameObject.Instantiate(wall, new Vector3(0, 4.5f, 0), new Quaternion(0,0,0,0));
-        firstWallBottom = GameObject.Instantiate(wall, new Vector3(0, -4.5f, 0), new Quaternion(0, 0, 0, 0));
+        GameObject firstWallTop = Instantiate(wall, new Vector3(0, 4.5f, 0), new Quaternion(0,0,0,0));
+        GameObject firstWallBottom = Instantiate(wall, new Vector3(0, -4.5f, 0), new Quaternion(0, 0, 0, 0));
 
         spawnedTopPosition.Add(firstWallTop);
         spawnedBottomPosition.Add(firstWallBottom);
     }
     private void Update()
     {
+        if (spawnedTopPosition[^1].transform.position.x < 20f)
+        {
+            GameObject wallTopInstance = Instantiate(wall, new Vector3(spawnedTopPosition[^1].transform.position.x + 17.8f, spawnedTopPosition[^1].transform.position.y, spawnedTopPosition[^1].transform.position.z), new Quaternion(0, 0, 0, 0));
+            GameObject wallBottomInstance = Instantiate(wall, new Vector3(spawnedBottomPosition[^1].transform.position.x + 17.8f, spawnedBottomPosition[^1].transform.position.y, spawnedBottomPosition[^1].transform.position.z), new Quaternion(0, 0, 0, 0));
+            spawnedTopPosition.Add(wallTopInstance);
+            spawnedBottomPosition.Add(wallBottomInstance);
+        }
+        if (spawnedTopPosition.Count > 4)
+        {
+            GameObject.Destroy(spawnedTopPosition[0]);
+            GameObject.Destroy(spawnedBottomPosition[0]);
+        }
     }
 }
