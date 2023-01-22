9e02b5b952517e7fab1867b950285d8d93a3714a
Tiago Braga <braga.tiagoxavier@gmail.com>
2023-01-21 22:49:59 -0300

    created moviment for walls

--- Assets/Runtime/Scripts/Walls/WallSpawn.cs
+++ Assets/Runtime/Scripts/Walls/WallSpawn.cs
@@ -11,10 +11,12 @@ public class WallSpawn : MonoBehaviour
     public GameObject firstWallTop;
     public GameObject firstWallBottom;
 
+    public float heightWalls = 4.5f;
+
     private void Start()
     {
-        GameObject firstWallTop = Instantiate(wall, new Vector3(0, 4.5f, 0), new Quaternion(0,0,0,0));
-        GameObject firstWallBottom = Instantiate(wall, new Vector3(0, -4.5f, 0), new Quaternion(0, 0, 0, 0));
+        GameObject firstWallTop = Instantiate(wall, new Vector3(0, heightWalls, 0), new Quaternion(0,0,0,0));
+        GameObject firstWallBottom = Instantiate(wall, new Vector3(0, -heightWalls, 0), new Quaternion(0, 0, 0, 0));
 
         spawnedTopPosition.Add(firstWallTop);
         spawnedBottomPosition.Add(firstWallBottom);
@@ -23,15 +25,21 @@ public class WallSpawn : MonoBehaviour
     {
         if (spawnedTopPosition[^1].transform.position.x < 20f)
         {
-            GameObject wallTopInstance = Instantiate(wall, new Vector3(spawnedTopPosition[^1].transform.position.x + 17.8f, spawnedTopPosition[^1].transform.position.y, spawnedTopPosition[^1].transform.position.z), new Quaternion(0, 0, 0, 0));
-            GameObject wallBottomInstance = Instantiate(wall, new Vector3(spawnedBottomPosition[^1].transform.position.x + 17.8f, spawnedBottomPosition[^1].transform.position.y, spawnedBottomPosition[^1].transform.position.z), new Quaternion(0, 0, 0, 0));
+            Vector3 transformLastWall = new Vector3(spawnedTopPosition[^1].transform.position.x, heightWalls, 0);
+            GameObject wallTopInstance = Instantiate(wall, new Vector3(transformLastWall.x + 17.8f, transformLastWall.y, transformLastWall.z), new Quaternion(0, 0, 0, 0));
+            GameObject wallBottomInstance = Instantiate(wall, new Vector3(transformLastWall.x + 17.8f, -transformLastWall.y, transformLastWall.z), new Quaternion(0, 0, 0, 0));
             spawnedTopPosition.Add(wallTopInstance);
             spawnedBottomPosition.Add(wallBottomInstance);
         }
         if (spawnedTopPosition.Count > 4)
         {
-            GameObject.Destroy(spawnedTopPosition[0]);
-            GameObject.Destroy(spawnedBottomPosition[0]);
+            firstWallTop = spawnedTopPosition[0];
+            firstWallBottom = spawnedBottomPosition[0];
+            GameObject.Destroy(firstWallTop);
+            GameObject.Destroy(firstWallBottom);
+            spawnedTopPosition.Remove(firstWallTop);
+            spawnedBottomPosition.Remove(firstWallBottom);
+            Debug.Log("Destroy");
         }
     }
 }
