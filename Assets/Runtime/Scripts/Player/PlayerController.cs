cd0aec86d34eb73696c4da902cefa9907294793e
Tiago Braga <braga.tiagoxavier@gmail.com>
2023-01-22 00:12:01 -0300

    Created jump for player

--- Assets/Runtime/Scripts/Player/PlayerController.cs
+++ Assets/Runtime/Scripts/Player/PlayerController.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputActions inputActions;
+    private Rigidbody2D rb;
+
+    public int jumpSpeed = 10;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        inputActions = new PlayerInputActions();
+        inputActions.PlayerControls.Enable();
+        inputActions.PlayerControls.Moviment.performed += onPlayerInput;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void onPlayerInput(InputAction.CallbackContext obj)
     {
-        
+        if (obj.control.name == "w")
+        {
+            rb.velocity = new Vector2(0, jumpSpeed);
+        } else
+        {
+            Debug.Log("Did not press");
+        }
     }
 }
