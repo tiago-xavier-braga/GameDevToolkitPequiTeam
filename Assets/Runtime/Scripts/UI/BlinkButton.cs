c9b3761afbed81ada420650b5a2f55addfb1109c
Dfam92 <dfam92@hotmail.com>
2023-01-21 23:44:46 -0300

    DotTween and TmPro were installed, BlinkButton created

---
+++ Assets/Runtime/Scripts/UI/BlinkButton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+namespace InfinityRun.UI
+{
+    public class BlinkButton : MonoBehaviour
+    {
+
+        [SerializeField] float blinkTime;
+        [SerializeField] float blinkFrequence;
+        [SerializeField] Image buttonImage;
+        [SerializeField] Color defaultColor;
+
+        private void Start()
+        {
+            defaultColor = buttonImage.color;
+            Blink();
+        }
+
+        private void Blink()
+        {
+            StartCoroutine(BlinkButtonDelayed());
+        }
+
+        IEnumerator BlinkButtonDelayed()
+        {
+            buttonImage.DOColor(defaultColor, blinkFrequence);
+            yield return new WaitForSeconds(blinkTime);
+            buttonImage.DOColor(Color.black, blinkFrequence);
+            yield return new WaitForSeconds(blinkTime);
+            Blink();
+
+        }
+    }
+}
+
+
